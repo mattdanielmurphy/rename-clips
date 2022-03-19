f7f804eeda8d70c4a742dc095b0232bb0685fc38
Matthew Murphy <matt@mattmurphy.ca>
2022-03-19 09:12:26 -0600

    re-add number correction for session 2

--- src/one-offs/zip-stems-samples.ts
+++ src/one-offs/zip-stems-samples.ts
@@ -141,7 +141,8 @@ async function zipStems() {
 			sampleFilename,
 		)
 
-		const correctedGhostNumber = uncorrectedGhostNumber
+		const correctedGhostNumber = String(+uncorrectedGhostNumber - 700)
+		// const correctedGhostNumber = uncorrectedGhostNumber
 
 		const renamedSample = renameStemOrSample(
 			sampleFilename,
